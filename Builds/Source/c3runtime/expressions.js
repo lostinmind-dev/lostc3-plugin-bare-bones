effaac8f9d6d7366f94a17665c2472982a19ca8f
Nikita <61884745+dakln@users.noreply.github.com>
2024-10-17 19:31:27 +0500

    final

--- Builds/Source/c3runtime/expressions.js
+++
@@ -1,4 +0,0 @@
-const ADDON_ID = "Lost_MyAddon";
-const C3 = globalThis.C3;
-C3.Plugins[ADDON_ID].Exps = {
-};
--- Builds/Source/c3runtime/plugin.js
+++
@@ -1,7 +0,0 @@
-const ADDON_ID = "Lost_MyAddon";
-const C3 = globalThis.C3;
-C3.Plugins[ADDON_ID] = class LostPlugin extends globalThis.ISDKPluginBase {
-    constructor() {
-        super();
-    }
-};
