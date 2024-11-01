f6c2438125c503b0f2e4e41379130115080a76f3
Nikita <61884745+dakln@users.noreply.github.com>
2024-11-01 09:35:57 +0500

    Update lost.d.ts

--- Addon/Types/ts-defs/lost.d.ts
+++ Addon/Types/ts-defs/lost.d.ts
@@ -1,4 +1,4 @@
-import type { LostConfig, AddonType } from "jsr:@lost-c3/lib";
+import type { LostConfig, AddonType } from "jsr:@lost-c3/lib@1.2.4";
 
 declare global {
     const Config: LostConfig<AddonType>;
