ec70950338c0af3477cba60204bbe4faf028b3f1
Nikita <61884745+dakln@users.noreply.github.com>
2024-12-08 07:30:39 +0500

    3.2.3

--- deps.ts
+++ deps.ts
@@ -1,4 +1,8 @@
 export {
     defineConfig, defineAddon, Plugin, Behavior, Property,
     Category, Action, Condition, Expression, addParam, Param
-} from 'jsr:@lost-c3/lib@3.2.2';
+} from 'jsr:@lost-c3/lib@3.2.3';
+
+export {
+    bold, italic, underline, code, strikethrough
+} from 'jsr:@lost-c3/lib@3.2.3/misc';
