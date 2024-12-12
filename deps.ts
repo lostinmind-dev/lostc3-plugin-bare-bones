a6a4f702e0b922d8e3aebaca41b1a84098e0f758
Nikita <llostinmind@gmail.com>
2024-12-12 06:40:33 +0500

    3.3.0

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 export {
     defineConfig, defineAddon, Plugin, Behavior, Property,
     Category, Action, Condition, Expression, addParam, Param
-} from 'jsr:@lost-c3/lib@3.2.4';
+} from 'jsr:@lost-c3/lib@3.3.0';
 
 export {
     bold, italic, underline, code, strikethrough
-} from 'jsr:@lost-c3/lib@3.2.4/misc';
+} from 'jsr:@lost-c3/lib@3.3.0/misc';
