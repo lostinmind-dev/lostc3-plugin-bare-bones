f066dbc3a21d87b5dbb6d1c7824f01c422f23172
Nikita <llostinmind@gmail.com>
2024-12-15 12:26:35 +0500

    Update to 3.3.4

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 export {
     defineConfig, defineAddon, Plugin, Behavior, Property,
     Category, Action, Condition, Expression, addParam, Param
-} from 'jsr:@lost-c3/lib@3.3.0';
+} from 'jsr:@lost-c3/lib@3.3.4';
 
 export {
     bold, italic, underline, code, strikethrough
-} from 'jsr:@lost-c3/lib@3.3.0/misc';
+} from 'jsr:@lost-c3/lib@3.3.4/misc';
